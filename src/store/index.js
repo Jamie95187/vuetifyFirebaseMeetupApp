bfecd583de5f533209c4c367778a2e65a6466012
Jamie <jamiewong63@googlemail.com>
2020-08-19 02:06:05 +0100

    feat: user can create new meetup and add to meetup list (not yet linked to firebase api

--- src/store/index.js
+++ src/store/index.js
@@ -24,8 +24,23 @@ export default new Vuex.Store({
     }
   },
   mutations: {
+    createMeetup (state, payload) {
+      state.meetups.push(payload)
+    }
   },
   actions: {
+    createMeetup ( {commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: '1231231231225asdfas'
+      }
+      // Reach out to firebase and store it
+      commit('createMeetup', meetup)
+    }
   },
   modules: {
   },
--- src/views/Meetup/CreateMeetup.vue
+++ src/views/Meetup/CreateMeetup.vue
@@ -1,12 +1,106 @@
 <template>
-  <div>
-    <p>The Create Meetup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h3 class="primary--text">Create a new Meetup</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                id="title"
+                v-model="title"
+                >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="image-url"
+                v-model="imageUrl"
+                >
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" height="300">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-textarea
+                name="description"
+                label="Description"
+                id="description"
+                v-model="description"
+                >
+              </v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+                class="primary"
+                :disabled="!formIsValid"
+                type="submit">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     name: 'CreateMeetup',
+    data: () => ({
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: ''
+    }),
+    computed: {
+      formIsValid () {
+        return this.title !== '' &&
+        this.location !== '' &&
+        this.imageUrl !== '' &&
+        this.description !== ''
+      }
+    },
+    methods: {
+      onCreateMeetup() {
+        const meetupData = {
+          title: this.title,
+          location: this.location,
+          imageUrl: this.imageUrl,
+          description: this.description,
+          date: new Date()
+        }
+        console.log("Created New Meetup!")
+        this.$store.dispatch('createMeetup', meetupData)
+      }
+    }
   }
 </script>
 
