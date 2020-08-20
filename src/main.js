6f315c2cd84677b444789fb5b1fbe06b352e991b
Jamie <jamiewong63@googlemail.com>
2020-08-20 01:11:04 +0100

    chore: setup firebase with project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import store from './store'
 import DateFilter from './filters/date'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -14,5 +15,14 @@ new Vue({
   router,
   vuetify,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCxxHZn6Xm-RWwjZvsG09cD0M2RC031aRQ",
+      authDomain: "meetup-c8d26.firebaseapp.com",
+      databaseURL: "https://meetup-c8d26.firebaseio.com",
+      projectId: "meetup-c8d26",
+      storageBucket: "meetup-c8d26.appspot.com"
+    })
+  }
 }).$mount('#app')
